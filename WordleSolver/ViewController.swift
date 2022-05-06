cf826aeb2f1a4ee007fe03be7cd5ec6209f83520
Brian Koga <brian.koga@wsu.edu>
2022-05-06 16:24:00 -0700

    Initial Commit

---
+++ WordleSolver/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WordleSolver
+//
+//  Created by Brian Koga on 5/6/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
